2bf1c4de1beee80c948b8bd05b2bc9c5d9a125c4
PedroRossi <pgrr@cin.ufpe.br>
2016-10-18 11:16:59 -0300

    Drag and doubleclick events working on the points

--- js/app/main.js
+++ js/app/main.js
@@ -1,11 +1,16 @@
-// Constante values for the program colors
-const colors = {
+// Color values
+var color = {
   background: 'grey',
-  lines: '',
-  curve: ''
+  curve: 'red',
+  line: 'white',
+  circle: 'blue'
 }
 // Control points array based on the user clicks
 var points = [];
+// Circles representing user clicks
+var circles = [];
+// The radius of the circle
+var circleSize = 10;
 // The real background with color
 var background = {};
 // The touchScreen for receiving the user click's
@@ -18,7 +23,7 @@ var bezier = {};
 var precision = 0.01;
 // Background setup
 background = new Rect(0, 0, stage.options.width, stage.options.height);
-background.attr('fillColor', 'grey');
+background.attr('fillColor', color.background);
 background.addTo(stage);
 // TouchScreen setup for ensuring
 // that the paths do not overleap
@@ -67,8 +72,13 @@ function casteljau() {
   bezierPoints.push(points[points.length-2]);
   bezierPoints.push(points[points.length-1]);
   bezier = new Path(bezierPoints)
-    .stroke('red',1);
-  stage.children([background, path, bezier, touchScreen]);
+    .stroke(color.curve,1);
+
+  var stageObjects = [background, path, bezier, touchScreen];
+  circles.forEach(function(c) {
+    stageObjects.push(c);
+  });
+  stage.children(stageObjects);
 }
 
 /**
@@ -76,15 +86,64 @@ function casteljau() {
  */
 function drawLine() {
   path = new Path(points)
-    .stroke('white',1);
-  casteljau();
+    .stroke(color.line,1);
+  if(points.length>4)
+    casteljau();
+  else {
+    var stageObjects = [background, path, touchScreen]
+    circles.forEach(function(circle) {
+      stageObjects.push(circle);
+    });
+    stage.children(stageObjects);
+  }
 }
 
 // When clicked generates new control point and
-// draws the line if it has more than 2 points
+// draws the line if it has more than 1 point
 touchScreen.on('pointerup', function(evt) {
   points.push(evt.x);
   points.push(evt.y);
-  if(points.length>4)
+  var c = new Circle(evt.x, evt.y, circleSize)
+    .fill(color.circle);
+
+  c.index = circles.length;
+  circles.push(c);
+
+  // When the circle is dragged around, the stage
+  // lower its precision for rendering real time
+  // the changes and after returns the precision
+  // to it's normal state and draw the new curve
+  c.on('drag', function(evt) {
+    var index = this.index;
+    circles[index]._attributes.x = evt.x;
+    circles[index]._attributes.y = evt.y;
+    points[index*2] = evt.x;
+    points[(index*2)+1] = evt.y;
+    if(points.length>2) {
+      precision = 0.1;
+      drawLine();
+      precision = 0.01;
+      drawLine();
+    }
+  });
+
+  // When the circle is doubleclicked, the circle and its
+  // respective point is removed from their array's of origin,
+  // we re-index the circles left and draw the lines
+  c.on('doubleclick', function(evt) {
+    for (var i = this.index+1; i < circles.length; i++) {
+      circles[i].index--;
+    }
+    circles.splice(this.index,1);
+    points.splice(2*this.index,2);
+    if(points.length>2)
+      drawLine();
+    else
+      stage.children([background, touchScreen, circles[0]]);
+  });
+
+  if(points.length>2)
     drawLine();
+  else
+    stage.addChild(c);
 });
