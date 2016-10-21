2d932b8ffcec913e33a6961b7be9730e4011160f
PedroRossi <pgrr@cin.ufpe.br>
2016-10-21 20:44:11 -0300

    Better undestanding of blossom area

--- js/app/main.js
+++ js/app/main.js
@@ -4,7 +4,11 @@ var color = {
   curve: 'red',
   line: 'white',
   circle: 'blue',
-  text: 'black'
+  text: 'black',
+  casteljau: {
+    circle: 'yellow',
+    line: 'yellow'
+  }
 }
 // Control points array based on the user clicks
 var points = [];
@@ -18,12 +22,12 @@ var circleSize = 5;
 var background = {};
 // The screen for touching
 var touchScreen = {};
-// The line path
 var path = {};
+// The line path
 // The bezier curve
 var bezier = {};
 // Precision value
-var precision = 0.01;
+var precision = 0.007;
 // Background setup
 background = new Rect(0, 0, stage.options.width, stage.options.height);
 background.attr('fillColor', color.background);
@@ -40,7 +44,7 @@ touchScreen.addTo(stage);
  * @param c2 is the second x or y
  */
 function getNewPoint(pos, c1, c2) {
-  return (Math.round((c1 - ((c1 - c2)*pos))*100))/100;
+  return (Math.round((c1 - ((c1 - c2)*pos))*1000))/1000;
 }
 
 /**
@@ -63,53 +67,30 @@ function getNewPath(pos, points) {
  * Gets the bezier points and draws the bezier curve
  */
 function casteljau() {
-  var bezierPoints = [];
   var paths = [];
-  var blossomTextPoints = [];
   // TODO PD later
   // console.log(paths[0]._segments);
   // console.log(paths);
   for (var i = 0; i <= 1; i+=precision) {
-    i = (Math.round(i*100))/100;
+    i = (Math.round(i*1000))/1000;
     var np = points;
     do {
-      np = getNewPath(i, np);
-      if(i == 0.5) {
-        var p = new Path(np).stroke('yellow',1);
-        paths.push(p);
-      }
+      np = getNewPath(Math.random(), np);
+      var p = new Path(np).stroke(color.casteljau.line,10);
+      paths.push(p);
     } while(np.length>4);
-    bezierPoints.push(getNewPoint(i, np[0], np[2]));
-    bezierPoints.push(getNewPoint(i, np[1], np[3]));
   }
-  // TODO Point numering system
-  // paths.forEach(function(p) {
-  //   for (var i = 0; i < p._segments.length; i++) {
-  //     var str = 'P'+i+(i+1);
-  //     var t = new Text(str).attr({
-  //       x: p._segments[i][1]-7,
-  //       y: p._segments[i][2]-7
-  //     });
-  //     blossomTextPoints.push(t);
-  //   }
-  // });
-  bezierPoints.push(points[points.length-2]);
-  bezierPoints.push(points[points.length-1]);
-  bezier = new Path(bezierPoints)
-    .stroke(color.curve,1);
-
-  var stageObjects = [background, path, bezier, touchScreen];
-  circles.forEach(function(c) {
-    stageObjects.push(c);
-  });
+  // Organizing the objects at the stage
+  var stageObjects = [background, path];
   paths.forEach(function(p) {
     stageObjects.push(p);
   });
   textPoints.forEach(function(t) {
     stageObjects.push(t);
   });
-  blossomTextPoints.forEach(function(b) {
-    stageObjects.push(b);
+  stageObjects.push(touchScreen);
+  circles.forEach(function(c) {
+    stageObjects.push(c);
   });
   stage.children(stageObjects);
 }
@@ -136,9 +117,10 @@ function drawLine() {
 
 // When clicked generates new control point and
 // draws the line if it has more than 1 point
-touchScreen.on('pointerup', function(evt) {
+touchScreen.on('click', function(evt) {
   points.push(evt.x);
   points.push(evt.y);
+
   var c = new Circle(evt.x, evt.y, circleSize)
     .fill(color.circle);
 
@@ -149,33 +131,9 @@ touchScreen.on('pointerup', function(evt) {
       y: evt.y-7
     });
 
-  t.index = c.index = circles.length;
+  c.index = circles.length;
   circles.push(c);
   textPoints.push(t);
-
-
-
-  // When the circle is dragged around, the stage
-  // lower its precision for rendering real time
-  // the changes and after returns the precision
-  // to it's normal state and draw the new curve
-  c.on('multi:drag', function(evt) {
-    var index = this.index;
-    circles[index]._attributes.x = evt.x;
-    circles[index]._attributes.y = evt.y;
-    points[index*2] = evt.x;
-    points[(index*2)+1] = evt.y;
-    textPoints[index].attr({
-      x: evt.x-7,
-      y: evt.y-7
-    });
-    if(points.length>2) {
-      // precision = 0.1;
-      drawLine();
-      // precision = 0.01;
-      // drawLine();
-    }
-  });
 
   // When the circle is doubleclicked, the circle and its
   // respective point is removed from their array's of origin,
@@ -183,6 +141,9 @@ touchScreen.on('pointerup', function(evt) {
   c.on('doubleclick', function(evt) {
     for (var i = this.index+1; i < circles.length; i++) {
       circles[i].index--;
+      textPoints[i].attr({
+        text: 'P'+(i-1)
+      });
     }
     circles.splice(this.index,1);
     textPoints.splice(this.index,1);
@@ -199,15 +160,11 @@ touchScreen.on('pointerup', function(evt) {
     stage.children([t, c, touchScreen]);
 });
 
-
-// var text = new Text('Hi');
-// text.attr({
-//   fontFamily: 'Arial, sans-serif',
-//   fontSize: 20,
-//   textFillColor: 'red',
-//   textStrokeColor: 'yellow',
-//   textStrokeWidth: 1,
-//   x: 50,
-//   y: 50
-// });
-// text.addTo(stage);
+// for (var i = 0; i < 100; i++) {
+//   var x = Math.random()*(Math.random()>0.5?1000:100);
+//   var y = (Math.random()*(Math.random()>0.5?1000:100));
+//   // console.log(x + ' ' + y);
+//   points.push(x);
+//   points.push(y);
+// }
+// drawLine();
